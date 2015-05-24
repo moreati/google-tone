b98da5e8ea204a8444ba11576353df3b4d70133c
Alex Willmer <alex@moreati.org.uk>
2015-05-24 02:09:47 +0100

    Sharpen frequency peaks, and zoom to audio frequencies/times

--- tones/spectrogram.py
+++ tones/spectrogram.py
@@ -21,7 +21,7 @@ def graph_spectrogram(wav_file):
     spylab.specgram(sound_info, Fs=frame_rate,
                     NFFT=4096, noverlap=4000,
                     )
-    x1, x2, y1, y2 = pylab.axis()
+    pylab.grid(True)
     pylab.axis((0.1, 1.85, 600, 2600))
     file_name, file_ext = os.path.splitext(wav_file)
     pylab.savefig('%s.%s' % (file_name, 'png'))
