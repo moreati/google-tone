42a8047035a33a30427cf8b56470911e1ba6448a
Alex Willmer <alex@moreati.org.uk>
2015-05-24 02:47:35 +0100

    Correct typo, reduce plot size, match y axis labels to expected peaks

--- tones/spectrogram.py
+++ tones/spectrogram.py
@@ -15,14 +15,16 @@ import pylab
 
 def graph_spectrogram(wav_file):
     sound_info, frame_rate = get_wav_info(wav_file)
-    pylab.figure(num=None, figsize=(19, 12))
+    pylab.figure(num=None, figsize=(8, 6))
     pylab.subplot(111)
     pylab.title('spectrogram of %r' % wav_file)
-    spylab.specgram(sound_info, Fs=frame_rate,
-                    NFFT=4096, noverlap=4000,
-                    )
+    pylab.specgram(sound_info, Fs=frame_rate,
+                   NFFT=4096, noverlap=4000,
+                   )
     pylab.grid(True)
     pylab.axis((0.1, 1.85, 600, 2600))
+    pylab.yticks([ 740.0,  830.6,  932.6, 1108.7, 1244.5,
+                  1480.0, 1661.2, 1864.66, 2217.46, 2489.0])
     file_name, file_ext = os.path.splitext(wav_file)
     pylab.savefig('%s.%s' % (file_name, 'png'))
 
